9a24bab60410c10671f001444ed2cefcc4baa87e
Hugo VERJUS <hugo.verjus@epita.fr>
2018-01-24 12:08:25 +0100

    reverted work done

---
+++ Code_Lyoko/Code_Lyoko/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Code_Lyoko
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Game1 game = new Game1();
+            game.Run();
+        }
+    }
+}
