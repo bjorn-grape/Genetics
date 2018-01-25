cd2901700989db55f715626f4a01e13a52476688
Hugo VERJUS <hugo.verjus@epita.fr>
2018-01-25 14:00:13 +0100

    Updated Tiles

--- Code_Lyoko/Code_Lyoko/Image.cs
+++ Code_Lyoko/Code_Lyoko/Image.cs
@@ -1,19 +1,31 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using System.Net.Mime;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Code_Lyoko
 {
     public class PlayerAppearance
     {
         private Texture2D texture_;
-        private int tile_width_;
-        private int tile_height_;
+        private int tile_size_;
         private int tile_number_x_;
         private int tile_number_y_;
         public string name_;
         public int tile_number;
-
-        PlayerAppearance(char path, int size, int x, int y)
+        
+        
+        /// <summary>
+        /// player tile constructor
+        /// </summary>
+        /// <param name="path"> Path to load</param>
+        /// <param name="x">number of columns on image</param>
+        /// <param name="y">number of rows on image</param>
+        /// <param name="size">size of a tile</param>
+        public PlayerAppearance(char path, int x = 16, int y = 1, int size = 128)
         {
+            tile_number_x_ = x;
+            tile_number_y_ = y;
+            tile_size_ = size;
+            //texture_ = Content.Load
             
         }
 
---
+++ Code_Lyoko/Code_Lyoko/Tile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Code_Lyoko
+{
+    public class Tile
+    {
+        protected Texture2D Texture;
+        protected Rectangle Rect;
+        private static ContentManager content;
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(Texture,Rect, Color.White);
+        }
+    }
+
+    public class CollisionTiles : Tile
+    {
+        public CollisionTiles(int i, Rectangle newRect)
+        {
+            this.Rect = newRect;
+            
+        }
+    }
+}
