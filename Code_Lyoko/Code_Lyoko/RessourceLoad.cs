2e44759cb7b5411c7c56a4fd32d796c77ed1869a
Hugo VERJUS <hugo.verjus@epita.fr>
2018-01-25 11:45:35 +0100

    Updated Load All map and print

--- Code_Lyoko/Code_Lyoko/RessourceLoad.cs
+++ Code_Lyoko/Code_Lyoko/RessourceLoad.cs
@@ -9,20 +9,30 @@ namespace Code_Lyoko
     {
         private static string map_path_;
         private static string img_path;
-        private static Dictionary<string,Map> maps_;
+        private static Dictionary<string, Map> maps_;
 
         public static void InitMap()
         {
-            maps_ = new Dictionary<string,Map>();
+            maps_ = new Dictionary<string, Map>();
             string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             path += "/../../map";
+            if (!Directory.Exists(path))
+                throw new Exception("Directory Doesn't exist");
             string[] files = Directory.GetFiles(path);
             foreach (var file in files)
             {
                 Map map = new Map(file);
                 maps_.Add(Path.GetFileNameWithoutExtension(file), map);
+                Console.WriteLine("Loaded map : " + Path.GetFileNameWithoutExtension(file));
             }
         }
-        
+
+        public static void PrintMaps()
+        {
+            foreach (var map in maps_)
+            {
+                map.Value.Print();
+            }
+        }
     }
 }
