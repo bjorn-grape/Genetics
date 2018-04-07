d52ca6f1844762c85aa0dbf08bea1f84fe30b25b
Hugo VERJUS <hugo.verjus@epita.fr>
2018-04-07 23:36:09 +0200

    Added specifications for program.cs

--- Code_Lyoko/Code_Lyoko/Program.cs
+++ Code_Lyoko/Code_Lyoko/Program.cs
@@ -10,69 +10,95 @@ namespace Code_Lyoko
 {
     internal class Program
     {
+        private const string PathForTest = "test.save";
+        private const string PathBotToSubmit = "../../save/bot.save";
+
         public static void Main(string[] args)
         {
             RessourceLoad.InitMap();
-            RessourceLoad.SetCurrentMap("long2");
-            //RessourceLoad.GenerateMap(3,20,50,60);
-            //PlayAsHuman();
-            //TrainFirstTime();
-            //Train();
-            Showbest();
-            //ShowNth(199);
+
+            RessourceLoad.SetCurrentMap("long"); //with this line you can set the current map from folder map
+
+            // Feel free to use all the function below in order to train your players
+            throw new NotImplementedException();
         }
 
-        static void TrainFirstTime()
+        /// <summary>
+        /// This function trains a population of 200 players by using new players at each generation
+        /// </summary>
+        /// <param name="n">number of generations you want to proceed</param>
+        static void TrainWithNew(int n)
         {
-            Factory.SetPathSave("testoftrain2.save");
+            Factory.SetPathSave(PathForTest);
             Factory.Init_new();
-            Factory.TrainWithNew(1);
-            Factory.PrintScore();
-            Factory.SaveState();
-            
-        }
-        
-        static void Train()
-        {
-            
-            Factory.SetPathLoadAndSave("testoftrain2.save");
-            Factory.Init();
-            Factory.Train(10);
+            Factory.TrainWithNew(n);
             Factory.PrintScore();
             Factory.SaveState();
         }
 
+        /// <summary>
+        /// This function trains a population of 200 players by duplicating and applying modification to the copy of 
+        /// the best players
+        /// </summary>
+        /// <param name="n">number of generations you want to proceed</param>
+        static void Train(int n)
+        {
+            Factory.SetPathLoadAndSave(PathForTest);
+            Factory.Init();
+            Factory.Train(n);
+            Factory.PrintScore();
+            Factory.SaveState();
+        }
+
+        /// <summary>
+        /// Show the current best player
+        /// </summary>
         static void Showbest()
         {
             Game1 game = new Game1();
-            Factory.SetPathLoadAndSave("testoftrain2.save");
+            Factory.SetPathLoadAndSave(PathForTest);
             Factory.Init();
             Factory.PrintScore(true);
             game.SetPlayer(Factory.GetBestPlayer());
-            
-            game.Run();
-        }
-        
-        static void ShowNth(int nth)
-        {
-            Game1 game = new Game1();
-            Factory.SetPathLoadAndSave("testoftrain2.save");
-            Factory.Init();
-            Factory.PrintScore(true);
-            game.SetPlayer(Factory.GetNthPlayer(nth));
-            
             game.Run();
         }
 
-        static void  PlayAsHuman()
+        /// <summary>
+        /// Show the nth player sorted by score in increasing order
+        /// </summary>
+        /// <param name="nth">player you want to access to, should be between 0 and 199</param>
+        static void ShowNth(int nth)
+        {
+            Game1 game = new Game1();
+            Factory.SetPathLoadAndSave(PathForTest);
+            Factory.Init();
+            Factory.PrintScore(true);
+            game.SetPlayer(Factory.GetNthPlayer(nth));
+
+            game.Run();
+        }
+
+        /// <summary>
+        /// This function allows you to try the game
+        /// </summary>
+        static void PlayAsHuman()
         {
             Game1 game = new Game1();
             Player player = new Player();
             player.SetStart(RessourceLoad.GetCurrentMap());
-            game.SetPlayer(player,true);
+            game.SetPlayer(player, true);
             game.Run();
         }
-        
-        
+
+        /// <summary>
+        /// save best player in folder save, you will be marked on this, so DON'T forget it!
+        /// </summary>
+        static void SaveBest()
+        {
+            Factory.SetPathLoad(PathForTest);
+            Factory.Init();
+            var soloList = new List<Player> {Factory.GetBestPlayer()};
+            SaveAndLoad.Save(PathBotToSubmit, soloList);
+        }
     }
 }
